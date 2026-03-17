4213dab8f2af58683d6a4eb93d2feef0cde8d6ec
Mario Barrita <flama@FLAMA.ipv6.infinitum.net.mx>
2026-03-17 00:02:13 -0600

    desactiva notificaciones para permitir emulación

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,14 @@ import 'services/auth.dart';
 import 'services/amplify_config.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';
+//import 'firebase_options.dart';
+//import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  //await Firebase.initializeApp(
+  //  options: DefaultFirebaseOptions.currentPlatform,
+  //);
   await dotenv.load(fileName: ".env");
   runApp(
     // Envolvemos toda la aplicación con ProviderScope para usar Riverpod
